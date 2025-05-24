b888d6e720944c3ee1cc7e3a633e5feae25f5aef
Aryan <Singh.ary.04@gmail.com>
2025-05-25 00:03:30 +0530

    API Enhancement

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,18 @@
 const API_KEY=a519eb6a46505c4f1cb1d41e4be08823;
-const BASE_URL="https://www.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
