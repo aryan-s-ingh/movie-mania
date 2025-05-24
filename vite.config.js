524cd529f7c7015e44d5967995cf1e11e8d9d658
Aryan <Singh.ary.04@gmail.com>
2025-05-25 01:01:24 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/movie%20mania/',
   plugins: [react()],
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
-  }
 })
